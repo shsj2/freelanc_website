a01a4fce5ec45b187b28283e5968939ab005daff
shsj2 <javaneh2288@gmail.com>
2025-02-22 12:28:23 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// اضافه کردن تعاملات جاوا اسکریپت در صورت نیاز
+// مدیریت فرم آپلود
+document.getElementById('upload-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('عکس و متن با موفقیت آپلود شد!');
+});
+
+// مدیریت فرم درخواست مشتری
+document.getElementById('request-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('درخواست شما با موفقیت ثبت شد!');
+});
